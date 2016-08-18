2403256cb5d4eabbff2f0e4e1d53009e02fe3b44
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-08-18 08:04:36 -0400

    fix cabal

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import Lib
+  import Lib
+  import Control.Exception(catch)
+  import System.Environment(getProgName)
+  import System.IO.Error(isUserError)
 
-main :: IO ()
-main = ui
+  main :: IO ()
+  main = ui `catch` handler
+
+  handler :: IOError -> IO ()
+  handler e
+    | isUserError e = do
+      progName <- getProgName
+      putStrLn ("Usage: " ++ progName ++ " 180")
+    | otherwise = ioError e
